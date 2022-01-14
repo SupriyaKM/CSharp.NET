c9da40eec897556aeb82bd6709b87673df3ad8c6
SupriyaKM <supriyaklp99@gmail.com>
2022-01-14 17:48:40 +0530

    Hands-on 4.2

---
+++ Assignment4.2/Assignment4.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+            int total = 3;
+
+            for (int i = 0; i < total; i++)
+            {
+                string name = Console.ReadLine();
+                int age = int.Parse(Console.ReadLine());
+
+                queue.Enqueue(new Person()
+                {
+                    Name = name,
+                    Age = age
+                });
+            }
+
+            for (int i = 0; i < total; i++)
+            {
+                Person p = (Person)queue.Dequeue();
+                Console.WriteLine(p.ToString());
+            }
+        }
+
+    }
+}
+
