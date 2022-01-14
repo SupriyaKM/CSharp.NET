0b147f9159e7d4df871fc01f68181747c9c3eac7
SupriyaKM <supriyaklp99@gmail.com>
2022-01-14 17:50:19 +0530

    Hands-on 4.3 submitted

---
+++ Assignment4.3/Assignment4.3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            int total = 3;
+
+            for (int i = 0; i < total; i++)
+            {
+                string name = Console.ReadLine();
+                int age = int.Parse(Console.ReadLine());
+
+                stack.Push(new Person()
+                {
+                    Name = name,
+                    Age = age
+                });
+            }
+
+            for (int i = 0; i < total; i++)
+            {
+                Person p = (Person)stack.Pop();
+                Console.WriteLine(p.ToString());
+            }
+        }
+
+    }
+}
+
