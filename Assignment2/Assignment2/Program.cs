4f3ac8f62fcaa43ba83cf8c4c0c2bc10ca84022f
SupriyaKM <supriyaklp99@gmail.com>
2022-01-14 17:43:30 +0530

    Hands-on2 submited

---
+++ Assignment2/Assignment2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Student s = new Student(1, "ABC", "c#");
+                s.Payment(1000);
+                s.Print();
+            Console.WriteLine("TotalFee:" + s.TotalFee);
+            Console.WriteLine("DueAmount:"+s.DueAmount);
+            }
+    }
+}
+
+
+   
---
+++ Assignment2/Assignment2/Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class Student
+    {
+        
+        private int rollno;
+        private string name;
+        private string course;
+        private int feepaid;
+
+        public Student(int rollno, string name, string course)
+        {
+            this.rollno = rollno;
+            this.name = name;
+            this.course = course;
+        }
+
+        public void Payment(int amount)
+        {
+            feepaid += amount;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("rollno:"+rollno);
+            Console.WriteLine("name:"+name);
+            Console.WriteLine("course:"+course);
+            Console.WriteLine("feepaid:"+feepaid);
+        }
+
+        public int DueAmount
+        {
+            get
+            { 
+                return TotalFee - feepaid;
+            }
+            
+        }
+
+        public int TotalFee
+        {
+            get
+            {
+                return course == "c#" ? 2000 : 3000;
+            }
+        }
+    }
+
+}
+
