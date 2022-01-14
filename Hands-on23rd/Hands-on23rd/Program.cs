12b50dc32e12aded785d245a64c7ab778c671c57
SupriyaKM <supriyaklp99@gmail.com>
2022-01-14 17:54:40 +0530

    Hands-on submitted

---
+++ Hands-on23rd/Hands-on23rd/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hands_on23rd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student(1, "Supriya", "c#");
+            s.Payment(1000);
+            s.Print();
+            Console.WriteLine("TotalFee:" + s.TotalFee);
+            Console.WriteLine("DueAmount:" + s.DueAmount);
+        }
+    }
+}
---
+++ Hands-on23rd/Hands-on23rd/Student.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hands_on23rd
+{
+    class Student
+    {
+        private int rollno;
+        private string name;
+        private string course;
+        private int feepaid;
+
+        private static double servicetax = 12.3;
+
+        public Student(int rollno, string name, string course)
+        {
+            this.rollno = rollno;
+            this.name = name;
+            this.course = course;
+        }
+
+        public void Payment(int amount)
+        {
+            feepaid += amount;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("RollNo:"+rollno);
+            Console.WriteLine("Name:"+name);
+            Console.WriteLine("Course:"+course);
+            Console.WriteLine("Feepaid:"+feepaid);
+        }
+
+        public int DueAmount
+        {
+
+            get
+            {
+                return TotalFee - feepaid;
+            }
+        }
+
+        public int TotalFee
+        {
+            get
+            {
+                double total = course == "c#" ? 2000 : 3000;
+                // service tax
+                total = total + total * (servicetax / 100);
+                return (int)total;
+            }
+        }
+
+        public static double ServiceTax
+        {
+            get
+            {
+                return servicetax;
+            }
+            set
+            {
+                servicetax = value;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+  
