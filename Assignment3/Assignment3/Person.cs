a1bd0c866522f9e71a5f70773b33f9a9c18ebf05
SupriyaKM <supriyaklp99@gmail.com>
2022-01-14 17:45:03 +0530

    Hands-on3 submitted

---
+++ Assignment3/Assignment3/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    class Person
+    {
+        protected string firstName;
+        protected string lastName;
+        protected int idNumber;
+
+        public Person(String firstName, String lastName, int identification)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.idNumber = identification;
+        }
+     
+        public void printPerson()
+        {
+            Console.WriteLine("Name: " + lastName + ", " + firstName + "\nID: " + idNumber);
+        }
+    }
+}
+
